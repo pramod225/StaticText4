83d95590d52ecc44dd1cd5269908b032186091b4
BaniYashu <BaniYashu@DESKTOP-C3CMV99>
2020-10-14 18:29:19 +0530

    Add project files.

---
+++ StaticClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticText4
+{
+    class Student
+    {
+        int StudentRollNo;
+        String StudentName;
+        static String college = "IITM";//static variable  
+                                      //constructor  
+        Student(int StudentRollNo, String StudentName)
+        {
+            StudentRollNo = Student;
+            StudentName = n;
+        }
+        //method to display the values  
+        void display() { System.out.println(rollno + " " + name + " " + college); }
+    }
+}
